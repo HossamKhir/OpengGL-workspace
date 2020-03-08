999f9908ed91ca90a503bc72c82fa9bfd92ec8c4
HossamKhir <hossam.khir1@outlook.com>
2020-03-08 14:13:57 +0200

    updated opengl-modern.vs

---
+++ opengl-modern.vs/opengl-modern.vs/rotating_square.h
@@ -0,0 +1,22 @@
+/*
+ * rotating_square.h
+ *
+ *  Created on: 25 Jan 2020
+ *      Author: hossam
+ */
+
+#ifndef ROTATING_SQUARE_H_
+#define ROTATING_SQUARE_H_
+
+#define	SCREEN_WIDTH	1920
+#define	SCREEN_HIEGHT	1080
+
+#include <iostream>
+#include <glad\glad.h>
+#include <GLFW/glfw3.h>
+#include <math.h>
+
+int
+rotating_square(void);
+
+#endif /* ROTATING_SQUARE_H_ */
